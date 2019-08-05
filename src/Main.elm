94e21641a970cfc9c60769bee742b491e8ccb364
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2019-08-05 16:37:17 -0500

    well, its a thing

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,9 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
+import Html.Attributes as Attr
+import Html.Events as Events
 import Random as R exposing (Generator)
 
 
@@ -58,11 +61,55 @@ capitalize str =
         |> Maybe.withDefault str
 
 
-dialogues : Generator (Html msg)
-dialogues =
-    R.map2 (\name line -> div [] [ span [] [ strong [] [ text (name ++ ": ") ], text line ] ])
+intros : Generator (Html msg)
+intros =
+    R.map2 say
         names
         quests
+
+
+say : String -> String -> Html msg
+say name line =
+    p [] [ span [] [ strong [] [ text (name ++ ": ") ], text line ] ]
+
+
+dialogues : Info msg -> Generator (Html msg)
+dialogues info =
+    R.map2 (\intro choice -> div [] [ intro, choice ])
+        intros
+        (choices info)
+
+
+choices : Info msg -> Generator (Html msg)
+choices info =
+    R.map3
+        (\yes no wat ->
+            case info.choice of
+                Just choice ->
+                    say "You"
+                        (case choice of
+                            Yes ->
+                                yes
+
+                            No ->
+                                no
+
+                            Wat ->
+                                wat
+                        )
+
+                Nothing ->
+                    p []
+                        [ button [ Events.onClick (info.choose Yes) ] [ text yes ]
+                        , span [] [ text "  " ]
+                        , button [ Events.onClick (info.choose No) ] [ text no ]
+                        , span [] [ text "  " ]
+                        , button [ Events.onClick (info.choose Wat) ] [ text wat ]
+                        ]
+        )
+        affirmativeResponses
+        negativeResponses
+        confusedResponses
 
 
 names : Generator String
@@ -104,6 +151,39 @@ folks =
         ]
 
 
+confusedResponses : Generator String
+confusedResponses =
+    R.uniform
+        "What?"
+        [ "Why me?"
+        , "That doesn't make sense."
+        , "Who are you?"
+        , "Huh?"
+        ]
+
+
+affirmativeResponses : Generator String
+affirmativeResponses =
+    R.uniform
+        "How can I help?"
+        [ "Okay!"
+        , "You got it."
+        , "I'm on it!"
+        ]
+
+
+negativeResponses : Generator String
+negativeResponses =
+    R.uniform
+        "Eh. Not really."
+        [ "I'd rather not."
+        , "Not today!"
+        , "Me no want do."
+        , "Nah."
+        , "Haha, no."
+        ]
+
+
 quests : Generator String
 quests =
     R.map4
@@ -126,8 +206,78 @@ quests =
         objects
 
 
+main : Program () Model Msg
 main =
-    123456890
-        |> R.initialSeed
-        |> R.step dialogues
-        |> Tuple.first
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+init : Model
+init =
+    { seed = "123"
+    , choice = Nothing
+    }
+
+
+type alias Model =
+    { seed : String
+    , choice : Maybe Choice
+    }
+
+
+type Choice
+    = Yes
+    | No
+    | Wat
+
+
+type Msg
+    = UpdateSeed String
+    | Choose Choice
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateSeed seed ->
+            { model | seed = seed }
+
+        Choose choice ->
+            { model | choice = Just choice }
+
+
+stringToInt : String -> Int
+stringToInt =
+    String.toList
+        >> List.map Char.toCode
+        >> List.sum
+
+
+type alias Info msg =
+    { choice : Maybe Choice
+    , choose : Choice -> msg
+    }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div []
+            [ label []
+                [ text "Seed: "
+                , input
+                    [ Events.onInput UpdateSeed
+                    , Attr.value model.seed
+                    ]
+                    []
+                ]
+            ]
+        , model.seed
+            |> stringToInt
+            |> R.initialSeed
+            |> R.step (dialogues { choice = model.choice, choose = Choose })
+            |> Tuple.first
+        ]
